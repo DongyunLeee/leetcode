e98f7b5863c4f0916f2a2ef366f3e77131e13f65
lee <speed1252@gmail.com>
2022-09-28 07:53:28 +0900

    [Solved] 23. Merge k Sorted Lists /* Using Linked List Solution */

---
+++ 23_1.py
@@ -0,0 +1,25 @@
+from typing import List, Optional
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+# Using Linked List Solution
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        data = []
+        for list in lists:
+            while list:
+                data.append(list.val)
+                list = list.next
+
+        data.sort()
+        tmp = ListNode(data[0])
+        head = tmp
+        for item in data[1:]:
+            tmp.next = ListNode(item)
+            tmp = tmp.next
+
+        return head
