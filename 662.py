538cd634f7509f3b8e406ff624d69a89ef1a103e
lee <speed1252@gmail.com>
2022-09-27 21:24:43 +0900

    [Solved] 622. Design Circular Queue

---
+++ 662.py
@@ -0,0 +1,49 @@
+class MyCircularQueue:
+
+    def __init__(self, k: int):
+        self.que = [None] * k
+        self.front = 0
+        self.rear = -1
+        self.size = 0
+
+    def enQueue(self, value: int) -> bool:
+        if self.size == len(self.que):
+            return False
+        self.rear += 1
+        self.rear = self.rear % len(self.que)
+        self.que[self.rear] = value
+        self.size += 1
+        return True
+
+    def deQueue(self) -> bool:
+        if self.size == 0:
+            return False
+        self.que[self.front] = None
+        self.front += 1
+        self.front = self.front % len(self.que)
+        self.size -= 1
+        return True
+
+    def Front(self) -> int:
+        if self.que[self.front] == None:
+            return -1
+        else:
+            return self.que[self.front]
+
+    def Rear(self) -> int:
+        if self.que[self.rear] == None:
+            return -1
+        else:
+            return self.que[self.rear]
+
+    def isEmpty(self) -> bool:
+        if self.size == 0:
+            return True
+        else:
+            return False
+
+    def isFull(self) -> bool:
+        if self.size == len(self.que):
+            return True
+        else:
+            return False
